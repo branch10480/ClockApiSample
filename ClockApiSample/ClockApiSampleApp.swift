8f74428eb8e6bbd2030a03342a2ea0198c59b4dd
branch10480 <toshiharu.imaeda@gmail.com>
2022-11-27 18:29:07 +0900

    Initial Commit

---
+++ ClockApiSample/ClockApiSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApiSampleApp.swift
+//  ClockApiSample
+//
+//  Created by Toshiharu Imaeda on 2022/11/27.
+//
+
+import SwiftUI
+
+@main
+struct ClockApiSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
